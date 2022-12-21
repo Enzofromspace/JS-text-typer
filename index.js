7db484944aab0bd1ab00691f750c95d1013803db
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-21 17:18:12 +0300

    Got text to load properly - testing recursive loop

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()){
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function draw(element){
-  console.log(element);
+//async loop
+async function draw(el){
+  const text = el.textContent;
+  let soFar = '';
+  for(const letter of text){
+    //console.log(letter);
+    soFar += letter;
+    el.textContent = soFar; 
+    //wait a bit
+    const {typeMin, typeMax} = el.dataset;
+    const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+    await wait(amountOfTimeToWait); 
+  }
 }
 
-const els = document.querySelectorAll('[data-type]');
+//recursive loop
+
+document.querySelectorAll('[data-type]').forEach(draw);
