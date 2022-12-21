ca835375dfb963c6a119f724f8160bfe8941fd69
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-21 17:04:23 +0300

    setup utiliy functions

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # JS-text-typer
+A Text Typer that varies the speed at which text is typed.
+
+Might turn this into a pixel charater project where text is displayed with sound like the indie game Owl Boy.
+
+Project based on exercise from Beginner Javascript by Wes Bos: https://wesbos.com/courses
+
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-m2lzaz)
--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Import stylesheets
 import './style.css';
-
 // Write Javascript code!
+function wait(ms = 0){
+  return new Promise(resolve => setTimeout(resolve, ms)); 
+}
+
+function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()){
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function draw(element){
+  console.log(element);
+}
+
+const els = document.querySelectorAll('[data-type]');
